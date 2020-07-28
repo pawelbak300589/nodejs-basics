8187ecbc2116a1d765dd252c4b7c0c41e27aaec7
Pawel Bak <pawelbak300589@gmail.com>
2020-07-28 14:51:13 +0100

    Update express routes

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('second middleware');
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.get('/', (req, res, next) => {
     res.send('<h1>Hello from Express middleware!</h1>');
 });
 
