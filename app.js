c7de611956b103e84d449b97675f2d78675b701d
Pawel Bak <pawelbak300589@gmail.com>
2020-07-28 14:28:13 +0100

    Repleace vanilla node createServer & listen with express listen method

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -14,8 +12,6 @@ app.use((req, res, next) => {
     res.send('<h1>Hello from Express middleware!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(4000, () => {
+app.listen(4000, () => {
     console.log('Listening on 4000');
 });
