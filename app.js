7bcfd0f644d9d179b209a01bc871e6d07eeb2249
Pawel Bak <pawelbak300589@gmail.com>
2020-07-28 10:48:19 +0100

    Add routing and redirect requests

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
 
     res.setHeader('Content-Type', 'text/html');
+
+    if (url === '/') {
+        res.write('<html lang="en">');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.write('<html lang="en">');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
